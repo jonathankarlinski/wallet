8f1bb52260cc3ccfd12eb9dbeebba3e25e998699
Jonathan <jonathankarlinski57@gmail.com>
2022-08-04 13:43:14 -0300

    Requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,19 +1,82 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Table extends Component {
+class Table extends Component {
   render() {
+    const { userCoin } = this.props;
+    console.log(userCoin);
     return (
-      <header>
-        <th>Descrição:</th>
-        <th>Tag:</th>
-        <th>Método de pagamento:</th>
-        <th>Valor:</th>
-        <th>Moeda:</th>
-        <th>Câmbio utilizado:</th>
-        <th>Valor convertido:</th>
-        <th>Moeda de conversão:</th>
-        <th>Editar/Excluir:</th>
-      </header>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              userCoin.map((coin) => (
+                <tr
+                  key={ coin.id }
+                >
+                  <td>
+                    {coin.description}
+                  </td>
+                  <td>
+                    {coin.tag}
+                  </td>
+                  <td>
+                    {coin.method}
+                  </td>
+                  <td>
+                    {coin.value}
+                  </td>
+                  <td>
+                    {coin.currency}
+                  </td>
+                  <td>
+                    {
+                      coin.exchangeRates[coin.currency].ask
+                    }
+                    {/* descobrir qual é o cambio */}
+                  </td>
+                  <td>
+                    {
+                      coin.exchangeRates[coin.currency]
+                        .ask * Number(coin.value)
+                    }
+                  </td>
+                  <td>
+                    Real
+                  </td>
+                  <td>
+                    <button type="button">+</button>
+                    <button type="button">-</button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
+const mapStateToProps = (state) => ({
+  userCoin: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  userCoin: PropTypes.arrayOf(Object),
+}.isRequired;
+
+export default connect(mapStateToProps)(Table);
