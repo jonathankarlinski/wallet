d451cec55eada1541e6e962700cbae84f99c7a6c
Jonathan <jonathankarlinski57@gmail.com>
2023-07-27 00:29:00 -0300

    feat: Publicando o projeto usando github pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, HashRouter } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route path="/carteira" component={ Wallet } />
-    </Switch>
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+      </Switch>
+    </HashRouter>
   );
 }
 
